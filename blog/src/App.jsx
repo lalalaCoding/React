db4cd7ec0a60fcd009687e9fc458d8d6a1d43666
codemyungja <codemyungja@naver.com>
2024-12-19 11:57:07 +0900

    jsx 기초 문법

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -4,31 +4,17 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  let post = '강남 우동 맛집';
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>프로젝트</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+        <div className="black-nav">
+          <h4 style={{color : 'red', fontSize: '16px'}}>블로그</h4>
+        </div>
+
+        <h4>{ post }</h4>
+    </div>
   )
 }
 
