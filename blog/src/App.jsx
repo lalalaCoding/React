bbac85699b46170400f8c559b7a13249fe169f66
codemyungja <codemyungja@naver.com>
2024-12-21 11:50:41 +0900

    Props

--- blog/src/App.jsx
+++ blog/src/App.jsx
@@ -10,15 +10,11 @@ function App() {
   let [따봉, 따봉변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
 
-  /*  배열.map(콜백함수)
-    1. 배열의 길이 만큼 콜백함수 실행
-    2. 콜백함수의 매개변수로 배열의 값을 전달받을 수 있다.
-    3. 콜백함수의 리턴값을 새로운 배열에 저장해준다. (새로운 배열 길이 = 원본 배열 길이)
-  
-  [1, 2, 3].map(function(a) {
-    return '123';
-  })
-  */
+  const 여자 = () => {
+    let copy = [...글제목]; // 깊은 복사, ...(나열 연산자)
+    copy[0] = '여자 코트 추천';
+    글제목변경( copy );
+  }
 
   return (
     <div className="App">
@@ -26,11 +22,7 @@ function App() {
           <h4>ReactBlog</h4>
         </div>
 
-        <button onClick = { () => {         
-          let copy = [...글제목]; // 깊은 복사, ...(나열 연산자)
-          copy[0] = '여자 코트 추천';
-          글제목변경( copy );
-        } }>제목 변경 버튼</button>
+        <button onClick = { 여자 }>제목 변경 버튼</button>
 
         <button onClick = { () => { 
           let sort = [...글제목].sort(); //깊은 복사
@@ -38,25 +30,7 @@ function App() {
          }}>
           가나다 정렬 버튼
         </button>
-
-        {/* <div className="list">
-          <h4>{ 글제목[0] } 
-            <span onClick={ () => { 따봉변경(따봉+1) } }>👍</span> {따봉} </h4>
-          <p>12월 19일 발행</p> 
-        </div>
-
-        <div className="list">
-          <h4>{ 글제목[1] }</h4>
-          <p>12월 19일 발행</p>
-        </div>  
-
-        <div className="list">
-          <h4 onClick = { () => { 
-            setModal(!modal)
-           }}>{ 글제목[2] }</h4>
-          <p>12월 19일 발행</p>
-        </div>           */}
-        
+          
         {
           글제목.map(function(a, i) {
             return (
@@ -77,7 +51,7 @@ function App() {
         }
 
         {
-          modal == true ? <Modal/> : null
+          modal == true ? <Modal 글제목={글제목} color="yellow" 여자={여자}/> : null
         }
         
        
@@ -86,28 +60,16 @@ function App() {
   )
 }
 
-function Modal() {
+
+function Modal(props) {
   return (
-    <div className="modal">
-      <h4>제목</h4>
+    <div className="modal" style={{background: props.color}}>
+      <h4>{props.글제목[0]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={props.여자}>글수정</button>
     </div>
   )
 }
-
-// const MyComponent = () => {
-//   return (
-//     <ul>
-//       <li>이발하기</li>
-//       <li>장보기</li>
-//       <li>정보처리기사 복습</li>
-//     </ul>
-//   )
-// }
-
-
-
-
 
 export default App
